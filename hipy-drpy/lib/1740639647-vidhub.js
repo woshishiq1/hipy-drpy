1a799c0870fe8af8e26c60532f9586ccf8496ee8
woshishiq1 <1987669587@qq.com>
2025-03-02 20:44:41 +0800

    0302 2044

---
+++ hipy-drpy/lib/1740639647-vidhub.js
@@ -0,0 +1,31 @@
+var rule = {
+  title: 'vidhub',
+  //author yuluoxc
+  host: 'https://vidhub.me/',
+  url: '/vodshow/fyclass--------fypage---.html',
+  searchable: 0,
+  quickSearch: 0,
+  filterable: 0,
+  headers: {
+    'User-Agent': 'MOBILE_UA',
+  },
+  timeout: 5000,
+  class_name:'电影&电视剧&动漫&综艺',
+    class_url:'1&2&4&3',
+  play_parse: true,
+  lazy: "js:\n  let html = request(input);\n  let hconf = html.match(/r player_.*?=(.*?)</)[1];\n  let json = JSON5.parse(hconf);\n  let url = json.url;\n  if (json.encrypt == '1') {\n    url = unescape(url);\n  } else if (json.encrypt == '2') {\n    url = unescape(base64Decode(url));\n  }\n  if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {\n    input = {\n      parse: 0,\n      jx: 0,\n      url: url,\n    };\n  } else {\n    input;\n  }",
+  limit: 6,
+  double: false,
+  推荐: '.module-items&&.module-item;.module-item-titlebox&&a&&title;.module-item-cover&&.module-item-pic&&img&&data-src;.module-item-text&&Text;.module-item-titlebox&&a&&href',
+  一级: '.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+  二级: {
+    title: 'h1&&Text;.tag-link&&Text',
+    img: '.module-item-pic&&img&&data-src',
+    desc: '.video-info-items:eq(3)&&Text;.tag-link:eq(2)&&Text;.tag-link:eq(1)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(0)&&Text',
+    content: '.vod_content&&Text',
+    tabs: '.module-tab-item',
+    lists: '.module-player-list:eq(#id)&&.scroll-content&&a',
+    tab_text: 'div--small&&Text',
+  },
+  搜索: '.module-items .module-search-item;a&&title;img&&data-src;.video-serial&&Text;a&&href',
+}

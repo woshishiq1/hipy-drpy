1343c0689332d4eddadcfd0dafec33b9dc51f6aa
woshishiq1 <90903172+woshishiq1@users.noreply.github.com>
2025-12-30 18:48:43 +0800

    Delete AA999OK directory

--- AA999OK/CK/无忧/JS/js/策驰影院(自动).js
+++
@@ -1,8 +0,0 @@
-var rule = {
-  模板:'自动',
-  title:'策驰影院',
-  host:'https://www.cecidy.cc',
-  url:'/vodshow/fyclass--------fypage---/',
-  class_parse: '.nav-list&&li;a&&Text;a&&href;/vodtype/(.*?)/',
-  searchUrl: '/vodsearch/**----------fypage---/',
-}
